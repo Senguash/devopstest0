83b8a9e39513cda8891180f53cbf028806594c51
Senguash <60571135+Senguash@users.noreply.github.com>
2022-08-30 10:54:24 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn devops link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn devops/i);
   expect(linkElement).toBeInTheDocument();
+
+});
+
+test('has login button',()=>{
+  render(<App />);
+  let button = screen.getByRole("button");
+  expect(button).toHaveTextContent("Login");
 });
